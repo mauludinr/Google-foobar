e0db6a3c59e8b57546f2d09d8b8455ddab0c0c37
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-07-30 21:56:44 +0700

    Update main.py

--- challenges/braille-translation/main.py
+++ challenges/braille-translation/main.py
@@ -1,6 +1,6 @@
 import solution
 
 if __name__ == '__main__':
-  print(solution("code"))
-  print(solution("Braille"))
-  print(solution("The quick brown fox jumps over the lazy dog"))
+  print(solution.solution("code"))
+  print(solution.solution("Braille"))
+  print(solution.solution("The quick brown fox jumps over the lazy dog"))
