e5ac1efb5c0473d9fa08b915275bcdbc0fd1101e
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-08-20 07:25:25 +0700

    Update main.py

--- challenges/ion-flux-relabeling/main.py
+++ challenges/ion-flux-relabeling/main.py
@@ -1,5 +1,7 @@
 import solution
 
 if __name__ == '__main__':
-#   Input 1
-  print(solution.solution())
+# Input 1
+  print(solution.solution(3, [7, 3, 5, 1]))
+# Input 2
+  print(solution.solution(5, [19, 14, 28])))
